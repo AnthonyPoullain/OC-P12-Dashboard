623f29aa57835b95f97d9f24c5c7d0b46614b223
Anthony <anthonypoullain.pro@gmail.com>
2022-10-03 11:16:25 +0200

    :wrench: Configure .env variables

--- src/services/getUserInfo.js
+++ src/services/getUserInfo.js
@@ -1,7 +1,9 @@
-const BASE_URL = '';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const getUserInfo = async (id) => {
   const response = await fetch(`${BASE_URL}/user/${id}`);
+  console.log(BASE_URL);
+  console.log(response);
   return response.json();
 };
 
